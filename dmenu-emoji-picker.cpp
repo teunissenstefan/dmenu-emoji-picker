da3d903be1e97d1e31341d0b6e7412669a72e905
Stefan Teunissen <stefanteunissen1@gmail.com>
2020-07-25 19:11:05 +0200

    Don't copy anything if the dmenu result does not exist in the emoji file

--- dmenu-emoji-picker.cpp
+++ dmenu-emoji-picker.cpp
@@ -6,6 +6,11 @@
 #include <array>
 #include <algorithm>
 #include <string>
+#include <sstream>
+
+std::string notificationExecutable = "notify-send";
+std::string dmenuCommand = "dmenu-custom -i -l 10 -p 'Emoji: '";
+std::string emojiFileLocation = "~/repos/dmenu-emoji-picker/emoji";
 
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
@@ -29,15 +34,29 @@ int copy(std::string str){
 
 int main() {
 	std::string notificationCommand;
-	std::string text = exec("cat ~/repos/dmenu-emoji-picker/emoji | dmenu-custom -i -l 10 -p 'Emoji: '");
+	std::string text = exec(("cat " + emojiFileLocation + " | " + dmenuCommand).c_str());
 	text.erase(std::remove(text.begin(), text.end(), '\n'), text.end()); // Remove any newlines, otherwise selecting the emoji will fail
 	//std::cout << "Result: " << text << "EOL" << std::endl;
-	
+
 	if(text.empty()){
-		notificationCommand = "notify-send -t 200 'Emoji selection cancelled'";
+		notificationCommand = notificationExecutable + " -t 200 'Emoji selection cancelled'";
     	system(notificationCommand.c_str());
 		return 0;
 	}
+
+    //Check if the result actually exists in the file
+    std::string catResult = exec(("cat " + emojiFileLocation).c_str());
+    std::stringstream ss(catResult);
+    std::string to;
+    bool found = false;
+    while(std::getline(ss,to,'\n')){
+        if(to == text){
+            found = true;
+        }
+    }
+    if(!found){
+        return 0;
+    }
 		
 	//int i = text.length() - 1;
     //while (i != 0 && !isspace(text[i]))
@@ -50,7 +69,7 @@ int main() {
 	
 	copy(emoji);
 
-	notificationCommand = "notify-send -t 200 '" + emoji + " copied!'";
+	notificationCommand = notificationExecutable + " -t 200 '" + emoji + " copied!'";
     system(notificationCommand.c_str());
     return 0;
 }
